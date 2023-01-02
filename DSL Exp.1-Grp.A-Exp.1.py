b9264a8b472c45b1b4436b7f16516a06e75e277d
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2023-01-02 16:53:54 +0530

    Update DSL Exp.1-Grp.A-Exp.1.py

--- DSL Exp.1-Grp.A-Exp.1.py
+++ DSL Exp.1-Grp.A-Exp.1.py
@@ -49,7 +49,7 @@ def sym_diff(lst1,lst2):
 
 def CB(lst1,lst2):
     lst3=intersection(lst1,lst2)
-    print("\n\nList of students who play both cricket and badminton is : ", lst3)
+    print("\nList of students who play both cricket and badminton is : ", lst3)
     return len(lst3)
 
 def eCeB(lst1,lst2):
@@ -59,12 +59,12 @@ def eCeB(lst1,lst2):
 
 def nCnB(lst1,lst2,lst3):
     lst4=diff(lst1,union(lst2,lst3))
-    print("\n\nList of students who play neither cricket nor badminton is : ",lst4)
+    print("\nList of students who play neither cricket nor badminton is : ",lst4)
     return len(lst4)
 
 def CBnF(lst1,lst2,lst3):
     lst4=diff(intersection(lst1,lst2),lst3)
-    print("\n\nList of students who play cricket and football but not badminton is : ",lst4)
+    print("\nList of students who play cricket and football but not badminton is : ",lst4)
     return len(lst4)
 
 SEComp = []
@@ -72,31 +72,31 @@ n = int(input("\nEnter number of students in SE COMP: "))
 print("Enter the names of",n,"students (Please press ENTER after entering each students name) :")
 for i in range(0, n):
     ele = input()
-    SEComp.append(ele)  # adding the element
+    SEComp.append(ele)  
 print("Original list of students in SEComp : " + str(SEComp))
 
 Cricket = []
-n = int(input("\n\nEnter number of students who play cricket : "))
+n = int(input("\nEnter number of students who play cricket : "))
 print("Enter the names of",n,"students who play cricket (Please press ENTER after entering each students name) :")
 for i in range(0, n):
     ele = input()
-    Cricket.append(ele)  # adding the element
+    Cricket.append(ele)  
 print("Original list of students playing cricket is :" +str(Cricket))
 Cricket=removeDuplicate(Cricket)
 print("The list of students playing cricket after removing duplicates : " +str(Cricket))
 
 Football = []
-n = int(input("\n\nEnter number of students who play football : "))
+n = int(input("\nEnter number of students who play football : "))
 print("Enter the name of",n,"students who play football (Please press ENTER after entering each students name) :")
 for i in range(0, n):
     ele = input()
-    Football.append(ele)  # adding the element
+    Football.append(ele) 
 print("Original list of students playing football :" +str(Football))
 Football=removeDuplicate(Football)
 print("The list of students playing football after removing duplicates : " +str(Football))
 
 Badminton = []
-n = int(input("\n\nEnter number of students who play badminton : "))
+n = int(input("\nEnter number of students who play badminton : "))
 print("Enter the name of",n,"students who play badminton (Please press ENTER after entering each students name) :")
 for i in range(0, n):
     ele = input()
@@ -107,7 +107,7 @@ print("The list of students playing badminton after removing duplicates : " +str
 
 flag=1
 while flag==1:
-    print("\n\n--------------------MENU--------------------\n")
+    print("\n--------------------MENU--------------------\n")
     print("1. List of students who play both cricket and badminton")
     print("2. List of students who play either cricket or badminton but not both")
     print("3. List of students who play neither cricket nor badminton")
@@ -116,7 +116,7 @@ while flag==1:
     ch=int(input("Enter your Choice (from 1 to 5) :"))
     if ch==1:
         print("Number of students who play both cricket and badminton : ", CB(Cricket,Badminton))
-        a = input("\n\nDo you want to continue (yes/no) :")
+        a = input("\nDo you want to continue (yes/no) :")
         if a == "yes":
             flag = 1
         else:
@@ -124,7 +124,7 @@ while flag==1:
             print("Thanks for using this program!")
     elif ch==2:
         print("Number of students who play either cricket or badminton but not both : ", eCeB(Cricket, Badminton))
-        a = input("\n\nDo you want to continue (yes/no) :")
+        a = input("\nDo you want to continue (yes/no) :")
         if a == "yes":
             flag = 1
         else:
@@ -132,7 +132,7 @@ while flag==1:
             print("Thanks for using this program!")
     elif ch==3:
         print("Number of students who play neither cricket nor badminton : ", nCnB(SEComp,Cricket,Badminton))
-        a = input("\n\nDo you want to continue (yes/no) :")
+        a = input("\nDo you want to continue (yes/no) :")
         if a == "yes":
             flag = 1
         else:
@@ -140,7 +140,7 @@ while flag==1:
             print("Thanks for using this program!")
     elif ch==4:
         print("Number of students who play cricket and football but not badminton : ", CBnF(Cricket,Football,Badminton))
-        a = input("\n\nDo you want to continue (yes/no) :")
+        a = input("\nDo you want to continue (yes/no) :")
         if a == "yes":
             flag = 1
         else:
@@ -151,7 +151,7 @@ while flag==1:
         print("Thanks for using this program!")
     else:
         print("!!Wrong Choice!! ")
-        a=input("\n\nDo you want to continue (yes/no) :")
+        a=input("\nDo you want to continue (yes/no) :")
         if a=="yes":
             flag=1
         else:
